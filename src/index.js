4d9995f26516d7e5cfd8fb671b57c1dc1afb3191
Thoma02 <tomi20020509@gmail.com>
2023-03-23 11:32:47 +0100

    First three containers with some basic functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const slider = document.querySelector("#shoe-slider-container");
+const slider = document.querySelector("#shoe-slider");
+
+// let isPressed = false;
+// let cursorX;
+
+// slider.addEventListener("mouseclick", (e) => {
+//     isPressed = true;
+//     cursorX = e.offsetX - shoeContainer.offsetLeft;
+//     slider.style.cursor = "grabbing";
+// });
+
+// slider.addEventListener("mousemove", (e) => {
+//     if (!isPressed) return;
+//     e.preventDefault();
+//     shoeContainer.style.left = `${e.offsetX - cursorX}px`
+// });
+
+const sneakers = document.getElementById("sneakers");
+const sports = document.getElementById("sports");
+const oxford = document.getElementById("oxford");
+const sale = document.getElementById("sale");
+
+const sliderStyles = getComputedStyle(slider, null);
+
+sneakers.addEventListener("click", () => {
+    slider.style.left = 0
+})
+
+sports.addEventListener("click", () => {
+    console.log(sliderStyles.left)
+    slider.style.left = "-1204px"
+})
+console.log(sports)
+
+oxford.addEventListener("click", () => {
+    console.log(sliderStyles.left)
+    slider.style.left = "-1806px"
+})
+// 577 + 25 + 577 = 1204 + 602px = 1806px + 602px
+
+sale.addEventListener("click", () => {
+    console.log(sliderStyles.left)
+    slider.style.left = "-2408px"
+})
+
+
+
+
+
