359461829f66109d9a208dee34bfd7cffc818cf5
Thoma02 <tomi20020509@gmail.com>
2023-03-28 16:56:24 +0200

    Now a rating can be given from 1 to 5 hearts

--- src/components/Main4/Favourite/Favourite.js
+++ src/components/Main4/Favourite/Favourite.js
@@ -1,40 +1,25 @@
 import React, {useState} from "react";
-// import "../../../App.css";
 
 export default function Favourite() {
 
-    const [clickedHeart, setclickedHeart] = useState(false);
-    const heart = document.querySelector('#heart-fav');
-
-    const handleMouseOver = () => {
-        if(!clickedHeart) {
-            heart.src = "./assets/main4-icons/heart-red.svg"
-        }
-    }
-
-    const handleMouseOut = () => {
-        if(!clickedHeart) {
-            heart.src = "./assets/main4-icons/heart-blue.svg"
-        }
-    }
+    const [favourited, setFavourited] = useState(false);
+    const [hovered, setHovered] = useState(false);
 
     const handleClick = () => {
-        if(!clickedHeart) {
-            setclickedHeart(true);
-            heart.src = "./assets/main4-icons/heart-red.svg"
-        } else if(clickedHeart) {
-            setclickedHeart(false);
-            heart.src = "./assets/main4-icons/heart-blue.svg"
+        if(!favourited) {
+            setFavourited(true);
+        } else if(!hovered) {
+            setHovered(true);
         }
     }
 
     return(
         <div id="favourite"
             onClick={() => handleClick()}
-            onMouseOver={() => handleMouseOver()}
-            onMouseOut={() => handleMouseOut()}
+            onMouseOver={() => setHovered(true)}
+            onMouseOut={() => setHovered(false)}
         >
-            <img id="heart-fav" src="./assets/main4-icons/heart-blue.svg" alt=""/>
+            <img id="heart-fav" src={`${hovered ? "./assets/main4-icons/heart-red.svg" : "./assets/main4-icons/heart-blue.svg"}`} alt=""/>
         </div>
     )
 }
